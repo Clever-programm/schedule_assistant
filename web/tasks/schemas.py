691c9d8271e4faf6f3b740bee231b51d997eed57
clover <tsaregorodtsevigorek@yandex.ru>
2025-08-30 00:14:53 +0400

    feat: tasks schemas

---
+++ web/tasks/schemas.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class CreateTaskSchema(BaseModel):
+    owner_id: int = Field(title="Owner id")
+    task_text: Optional[str] = Field(title="Text of user's task", default=None)
+
+class TaskSchema(CreateTaskSchema):
+    id: int = Field(title="Uniq task id")
+    is_done: bool = Field(title="Task is done", default=False)
